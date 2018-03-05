2fe63d4f1b9f913a936d5fda6c3e86c345934098
Johnathan Phan <johnp@cc-g.co.uk>
2018-03-05 21:01:20 +0000

    move CORS suppourt to nginx and out of app. nodejs does not play nice with nginx on cors https://stackoverflow.com/questions/37335301/enable-cors-on-node-js-app-with-nginx-proxy

--- 01-Microservices/01-security_microservice/app.js
+++ 01-Microservices/01-security_microservice/app.js
@@ -2,7 +2,6 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cors = require('cors');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose    = require('mongoose');
@@ -25,7 +24,6 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
