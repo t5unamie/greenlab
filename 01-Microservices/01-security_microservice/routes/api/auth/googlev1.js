d7367de9aa0797a08cdeb7dcd85e7050162ac819
Johnathan Phan <johnp@cc-g.co.uk>
2019-01-01 15:30:47 +0000

    adding google auth method

---
+++ 01-Microservices/01-security_microservice/routes/api/auth/googlev1.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+var url = require('url');
+var GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
+
+
+ 
+// Use the client id and secret you received when setting up your OAuth account
+var GOOGLE_CLIENT_ID = 'XXX_CLIENT_ID_XXX'
+var GOOGLE_CLIENT_SECRET = 'XXX_CLIENT_SECRET_XXX'
+
+passport.use(new GoogleStrategy({
+    clientID:     GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "https://sec-ms.green-labs.io/api/auth/googlev1/googleAuth",
+    passReqToCallback   : true
+  },
+  function(request, accessToken, refreshToken, profile, done) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
+
+router.get('/google', passport.authenticate('google', { scope: 
+      [ 'https://www.googleapis.com/auth/plus.login',
+      , 'https://www.googleapis.com/auth/plus.profile.emails.read' ] }
+));
+
+router.get('/googleAuth', , 
+    passport.authenticate( 'google', { 
+        successRedirect: '/auth/google/success',
+        failureRedirect: '/auth/google/failure'
+}));
+
+module.exports = router;
