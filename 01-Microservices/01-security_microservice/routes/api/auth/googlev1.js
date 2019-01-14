98a990ac55faf9b1219b647947dcfb215fa99d48
Johnathan Phan <johnp@cc-g.co.uk>
2019-01-14 17:32:48 +0000

    exp 5 render info from scope

--- 01-Microservices/01-security_microservice/routes/api/auth/googlev1.js
+++ 01-Microservices/01-security_microservice/routes/api/auth/googlev1.js
@@ -35,8 +35,7 @@ passport.use(new GoogleStrategy({
 
 router.get('/google', passport.authenticate('google', { scope: 
       [ 'https://www.googleapis.com/auth/plus.login',
-      'https://www.googleapis.com/auth/userinfo.email',
-      'https://www.googleapis.com/auth/userinfo.profile' ] }
+      'https://www.googleapis.com/auth/plus.profile.emails.read' ] }
 ));
 
 router.get('/googleAuth', 
