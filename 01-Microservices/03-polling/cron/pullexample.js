d77d5ffe6a1c6ca56105f9316cfc54e720603685
Johnathan Phan <johnp@cc-g.co.uk>
2019-04-15 14:55:31 +0100

    added requests to outside source in example

--- 01-Microservices/03-polling/cron/pullexample.js
+++ 01-Microservices/03-polling/cron/pullexample.js
@@ -1,6 +1,12 @@
 const cron = require("node-cron");
 const fs = require("fs");
+const request = require('request');
 
 cron.schedule("* * * * *", function() {
-      console.log("running a task every minute");
-    });
+     console.log("running a task every minute");  
+	 request('https://jsonplaceholder.typicode.com/posts', function (error, response, body) {
+  	 	console.error('error:', error); // Print the error if one occurred
+  	 	console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  	 	console.log('body:', body); // Print the HTML for the Google homepage.
+	});
+});
